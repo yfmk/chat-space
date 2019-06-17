1639913c34a06d7edddb1046081258e09cd0cb9d
yfmk <cantocantocantocanto@gmail.com>
2019-06-17 13:56:09 +0900

    make file

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,6 @@ class Api::MessagesController < ApplicationController
   before_action :set_group
 
   def index
-    # binding.pry
     respond_to do |format|
       format.json{@messages = @group.messages.where('id > ?',params[:id]) }
     end
