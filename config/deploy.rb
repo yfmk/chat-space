46804f57014f5327e88416712779e7152031e2a6
yfmk <cantocantocantocanto@gmail.com>
2019-06-21 20:34:46 +0900

    make file

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid only for current version of Capistrano
+lock '3.11.0'
+
+set :application, ''
+set :repo_url,  'git@github.com:yfmk/chat-space.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['ssh -i RDQDk9HpWL9kLKD.pem ec2-user@3.113.149.160']  ※例：~/.ssh/key_pem.pem
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
