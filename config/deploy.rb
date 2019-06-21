a164691b0956effec17a373b596ea60a906be600
yfmk <cantocantocantocanto@gmail.com>
2019-06-21 21:45:59 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock 'Capistrano Version: 3.11.0'
 
-set :application, ''
+set :application, 'chat-space'
 set :repo_url,  'git@github.com:yfmk/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
